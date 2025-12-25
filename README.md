5b083b41bdfb57a9dad668f31f2f2cc3e38235a0
Yve-android-blacky <yvonne26sa90@gmail.com>
2025-12-25 22:16:21 +0100

    Mega-Setup-Struktur für android-blacky-A

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# android-blacky-A
+
+Android-App (Variante A) für Blacky.
+Package: com.yve.android.blacky
---
+++ app/src/main/java/com/yve/android/blacky/MainActivity.kt
@@ -0,0 +1,9 @@
+﻿package com.yve.android.blacky
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
